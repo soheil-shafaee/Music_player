008d6d55803ca6db53c461985f2c9931f2e058f3
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2023-11-28 14:46:14 +0330

    Define Our Widgets

--- music_player.py
+++ music_player.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QSlider, QPushButton, QDial
+from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5 import uic
 import sys
 
@@ -9,6 +10,23 @@ class UI(QMainWindow):
 
         # Load The UI File
         uic.loadUi("music_player.ui", self)
+
+        # Define Our Widgets
+        self.musicName = self.findChild(QLabel, "musicName")
+        self.artistName = self.findChild(QLabel, "artistName")
+        self.musicTimer = self.findChild(QSlider, "musictimer")
+        self.startCount = self.findChild(QLabel, "counterStart")
+        self.endCount = self.findChild(QLabel, "counterEnd")
+        self.playPauseButton = self.findChild(QPushButton, "playButton")
+        self.forwardButton = self.findChild(QPushButton, "forwardButton")
+        self.backwardButton = self.findChild(QPushButton, "backwardButton")
+        self.shuffleButton = self.findChild(QPushButton, "shuffleButton")
+        self.repeatButton = self.findChild(QPushButton, "repeatButton")
+        self.volume = self.findChild(QDial, "volume")
+        self.volumeLabel = self.findChild(QLabel, "volumeLabel")
+        self.listButton = self.findChild(QPushButton, "listButton")
+
+        # 
 
         # Show The App
         self.show()
