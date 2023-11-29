1d2077a7eb1b2f5eec68483e48212f06376c69bc
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2023-11-29 11:57:31 +0330

    Added File for adding music to the app

---
+++ music_list.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QListWidget, QPushButton
+from PyQt5 import uic
+import sys
+
+
+class musicList(QMainWindow):
+    def __init__(self):
+        super(musicList, self).__init__()
+
+        # Load The File
+        uic.loadUi("music_list.ui", self)
+
+        # Define Our Widgets
+
+        # Show The App
+        self.show()
+
+
+# Initialize The App
+app = QApplication(sys.argv)
+UIWindow = musicList()
+app.exec_()
